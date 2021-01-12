a594010d1bc36edd7bfad87dc54c6e8e73f40e0e
Arboker <denislupookov@gmail.com>
2021-01-12 22:06:06 +0200

    format code

--- src/components/ImagePickerScreen.js
+++ src/components/ImagePickerScreen.js
@@ -14,9 +14,9 @@ import {
 } from 'react-native';
 import ImagePicker from 'react-native-image-picker';
 import AntIcon from "react-native-vector-icons/AntDesign";
-import { 
-  AdMobBanner, 
-  AdMobInterstitial, 
+import {
+  AdMobBanner,
+  AdMobInterstitial,
   PublisherBanner,
   AdMobRewarded
 } from 'react-native-admob'
@@ -62,33 +62,33 @@ class ImagePickerScreen extends Component {
       } else {
         const source = { uri: response.uri };
         this.setState({ imageSource: source });
-       
+
         this.setState({
           loading: true
         })
 
         console.log(response.type)
         // You can also display the image using data:
-  
+
         this.extractText(response.uri, response.type);
 
       }
     });
-   
+
   }
 
   extractText = async (imageSource, imageType) => {
 
-  
-   
+
+
     let subscriptionKey = ['YOUR_SUBSCRIPTIONKEY'];
     let endpoint = ['YOUR_ENDPOINT']
     if (!subscriptionKey) { throw new Error('Set your environment variables for your subscription key and endpoint.'); }
-    
+
     var uriBase = endpoint + "vision/v2.1/ocr";
 
     // Request parameters.
- 
+
 
     // Display the image.
     var sourceImageUrl = imageSource;
@@ -98,179 +98,174 @@ class ImagePickerScreen extends Component {
       type: imageType, // or photo.type
       name: 'testPhotoName'
     });
-   
+
     fetch(uriBase,
-{
- 
- method: 'POST',
- headers: 
- {
-  'Content-Type': 'multipart/form-data',
-     'Ocp-Apim-Subscription-Key': subscriptionKey,
- },
- body: data,
+      {
+
+        method: 'POST',
+        headers:
+        {
+          'Content-Type': 'multipart/form-data',
+          'Ocp-Apim-Subscription-Key': subscriptionKey,
+        },
+        body: data,
 
 
-}).then((response) => response.json()).then((data) =>
-{
+      }).then((response) => response.json()).then((data) => {
 
-  const responseData = JSON.stringify(data, null, 2);
+        const responseData = JSON.stringify(data, null, 2);
 
-  const parsed = JSON.parse(responseData);
+        const parsed = JSON.parse(responseData);
 
-  var array = [];
+        var array = [];
 
-  parsed.regions.map(item => {
-      item.lines.map(el => {
-          el.words.map(word => {
+        parsed.regions.map(item => {
+          item.lines.map(el => {
+            el.words.map(word => {
               array = array.concat(word.text);
+            })
           })
-      })
-  })
+        })
 
-const newData = Array.from(array, x => x).join(' ');
-
-
-  this.setState({
-    text: newData,
-    loading: false,
-    buttonShow: true
-  })
+        const newData = Array.from(array, x => x).join(' ');
 
 
+        this.setState({
+          text: newData,
+          loading: false,
+          buttonShow: true
+        })
 
-}).catch((error) =>
-{
- 
-  console.log(error)
 
-});
+
+      }).catch((error) => {
+
+        console.log(error)
+
+      });
   };
 
 
-remove() {
-  this.setState({
-    text: '',
-    imageSource: null,
-    buttonShow: false,
-    textAreaVisibile: false
-  })
-}
+  remove() {
+    this.setState({
+      text: '',
+      imageSource: null,
+      buttonShow: false,
+      textAreaVisibile: false
+    })
+  }
 
-onChangeText(newText) {
-  this.setState({
-    text: newText
-  })
-}
-
-
-
+  onChangeText(newText) {
+    this.setState({
+      text: newText
+    })
+  }
 
   render() {
     const { imageSource } = this.state;
     const { navigate } = this.props.navigation;
     return (
-      <View style={{flex: 1}}>
-      <ScrollView>
-      <View style={styles.container}>
-      { this.state.imageSource == null ? (
-        <View>
-         <Button onPress={this.selectImage} >
-          <View style={[styles.image, styles.imageContainer, !imageSource && styles.rounded]}>
-              <Text>Choose picture</Text>
-          </View>
-        </Button>
-    </View>
-        ) : (
- <Image style={styles.image} source={imageSource} />
-        )}
-        
-  <View style={{paddingTop: 15}}>
-    {this.state.loading === true ? (
-       <View>
-       <ActivityIndicator size="large" color="#1479d9" />
-     </View>
-    ) : (
-      <View>
-
-         {this.state.textAreaVisibile == true ? (
-  <View style={styles.textAreaContainer} >
-    <TextInput
-    style={{fontSize:18}}
-       onChangeText={newText => this.onChangeText(newText)}
-       value={this.state.text}
-      multiline={true}
-      autoFocus={true}
-    />
-  </View>
-) : (
-  <View>
-     <Text style={{fontSize:18}}>{this.state.text}</Text>
-    </View>
-)}
-      </View>
-    )}
-  </View>
-           
-      </View>
-
-{this.state.imageSource == null ? (
-  <View style={{ justifyContent: 'center',
-  alignItems: 'center',}}>
-    <AdMobBanner
-adSize="mediumRectangle"
-adUnitID="ca-app-pub-3371284819677164/2245308596"
-testDevices={[AdMobBanner.simulatorId]}
-onAdFailedToLoad={error => console.error(error)}
-/>
-</View>
-) : (
-  <View></View>
-)}
-
-
-      </ScrollView>
-
-
-      {
-         
-          this.state.buttonShow === true ? (
-           <View style={{ margin: 20, position: 'absolute', bottom: 0, right: 0, flexDirection: 'row'}}>
-
-{this.state.textAreaVisibile == false ? (
-  <AntIcon name='edit' style={{backgroundColor:'#1479d9', fontSize: 32, color: 'white',
-  padding: 10, borderRadius: 50, marginRight: 8}} onPress={() => {
-    this.setState({
-      textAreaVisibile: true
-    })
-   }}/>
-) : (
-  <View></View>
-)}
-
-<AntIcon name='close' style={{backgroundColor:'#1479d9', fontSize: 32, color: 'white',
-        padding: 10, borderRadius: 50, marginRight: 8}} onPress={() => {
-          this.remove();
-         }}/>
-
- <AntIcon name='arrowright' style={{backgroundColor:'#1479d9', fontSize: 32, color: 'white',
-        padding: 10, borderRadius: 50}} onPress={() => {
-          const data = this.state.text;
-          this.props.navigation.navigate("SpellChecker", {
-            text: data
-          });
-        }}/>
-           
-                 </View>
+      <View style={{ flex: 1 }}>
+        <ScrollView>
+          <View style={styles.container}>
+            {this.state.imageSource == null ? (
+              <View>
+                <Button onPress={this.selectImage} >
+                  <View style={[styles.image, styles.imageContainer, !imageSource && styles.rounded]}>
+                    <Text>Choose picture</Text>
+                  </View>
+                </Button>
+              </View>
             ) : (
+                <Image style={styles.image} source={imageSource} />
+              )}
+
+            <View style={{ paddingTop: 15 }}>
+              {this.state.loading === true ? (
+                <View>
+                  <ActivityIndicator size="large" color="#1479d9" />
+                </View>
+              ) : (
+                  <View>
+
+                    {this.state.textAreaVisibile == true ? (
+                      <View style={styles.textAreaContainer} >
+                        <TextInput
+                          style={{ fontSize: 18 }}
+                          onChangeText={newText => this.onChangeText(newText)}
+                          value={this.state.text}
+                          multiline={true}
+                          autoFocus={true}
+                        />
+                      </View>
+                    ) : (
+                        <View>
+                          <Text style={{ fontSize: 18 }}>{this.state.text}</Text>
+                        </View>
+                      )}
+                  </View>
+                )}
+            </View>
+
+          </View>
+
+          {this.state.imageSource == null ? (
+            <View style={{
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}>
+              <AdMobBanner
+                adSize="mediumRectangle"
+                adUnitID="ca-app-pub-3371284819677164/2245308596"
+                testDevices={[AdMobBanner.simulatorId]}
+                onAdFailedToLoad={error => console.error(error)}
+              />
+            </View>
+          ) : (
+              <View></View>
+            )}
+        </ScrollView>
+        {
+
+          this.state.buttonShow === true ? (
+            <View style={{ margin: 20, position: 'absolute', bottom: 0, right: 0, flexDirection: 'row' }}>
+
+              {this.state.textAreaVisibile == false ? (
+                <AntIcon name='edit' style={{
+                  backgroundColor: '#1479d9', fontSize: 32, color: 'white',
+                  padding: 10, borderRadius: 50, marginRight: 8
+                }} onPress={() => {
+                  this.setState({
+                    textAreaVisibile: true
+                  })
+                }} />
+              ) : (
+                  <View></View>
+                )}
+
+              <AntIcon name='close' style={{
+                backgroundColor: '#1479d9', fontSize: 32, color: 'white',
+                padding: 10, borderRadius: 50, marginRight: 8
+              }} onPress={() => {
+                this.remove();
+              }} />
+
+              <AntIcon name='arrowright' style={{
+                backgroundColor: '#1479d9', fontSize: 32, color: 'white',
+                padding: 10, borderRadius: 50
+              }} onPress={() => {
+                const data = this.state.text;
+                this.props.navigation.navigate("SpellChecker", {
+                  text: data
+                });
+              }} />
+
+            </View>
+          ) : (
               <View></View>
             )
-            }
-
-
-  
-
-</View>
+        }
+      </View>
     );
   }
 }
@@ -286,7 +281,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    padding:10,
+    padding: 10,
     marginTop: 10
   },
   title: {
--- src/components/SpellChecker.js
+++ src/components/SpellChecker.js
@@ -9,103 +9,91 @@ class SpellCheckScreen extends React.Component {
 
   _isMounted = false;
 
+  static navigationOptions = {
+    title: 'Spell Check',
+  };
+  constructor(props) {
+    super(props);
+    const { params } = this.props.navigation.state;
+    this.state = {
+      dataText: params ? params.text : '',
+      resultText: '',
+      visible: true
+    };
+    this.fadeAnimation = new Animated.Value(0)
+  }
+  componentDidMount() {
 
-    static navigationOptions = {
-        title: 'Spell Check',
-      };
-
-      constructor(props) {
-       
-
-        super(props);
-        const { params } = this.props.navigation.state;
-        this.state = {
-            dataText: params ? params.text : '',
-          resultText: '',
-          visible: true
-        };
-
-        this.fadeAnimation = new Animated.Value(0)
-
-      }
-
-      componentDidMount() {
-
-       
-
-        this._isMounted = true;
-
+    this._isMounted = true;
     var dataText = this.state.dataText;
 
     return fetch('YOUR_WEB_SITE_WITH_SPELL_CHECKER',
       {
-       
-       method: 'POST',
-       headers: 
-       {
-        'Content-Type': 'text/html',
-       },
-       body: dataText
-      
-      
-      }).then((response) => response.json()).then((data) =>
-      {
 
-      //   var removeLetter = shorter=="b" ? String(data).substr(1) : String(data).substr(2);
+        method: 'POST',
+        headers:
+        {
+          'Content-Type': 'text/html',
+        },
+        body: dataText
 
-      var newData = '';
 
-      data.map(el => newData = el );
+      }).then((response) => response.json()).then((data) => {
 
-      if (newData.substr(0, 2) == "b") {
-        newData = newData.substr(1);
-      }
+        // removing "b"
 
-     if (newData.substr(0, 2) == "be") {
-        newData = newData.substr(2);
-      }
+        var newData = '';
 
-      if (newData.charAt(newData.length - 1) == `'`) {
-        newData = newData.substr(0, newData.length - 1);
-      }
+        data.map(el => newData = el);
 
-      console.log(newData)
-
-      this.setState({
-        resultText: newData
-      })
-      
-      
-      }).catch((error) =>
-      {
-       console.log(error);
-      
-      });
-        };
-
-  
-        componentWillUnmount() {
-          this._isMounted = false;
+        if (newData.substr(0, 2) == "b") {
+          newData = newData.substr(1);
         }
 
-       
-         animationStart() {
-          Animated.timing(this.fadeAnimation, {
-            toValue: 1,
-            duration: 500,
-            useNativeDriver: true,      
-          }).start(() => {
-            setTimeout(() => {
-              Animated.timing(this.fadeAnimation, {
-                toValue: 0,
-                duration: 500,
-                useNativeDriver: true,      
-              }).start()
-            }, 1500)
-             
-          });
-         
-         }
+        if (newData.substr(0, 2) == "be") {
+          newData = newData.substr(2);
+        }
+
+        if (newData.charAt(newData.length - 1) == `'`) {
+          newData = newData.substr(0, newData.length - 1);
+        }
+
+        console.log(newData)
+
+        this.setState({
+          resultText: newData
+        })
+
+
+      }).catch((error) => {
+        console.log(error);
+
+      });
+  };
+
+
+  componentWillUnmount() {
+    this._isMounted = false;
+  }
+
+
+  animationStart() {
+    Animated.timing(this.fadeAnimation, {
+      toValue: 1,
+      duration: 500,
+      useNativeDriver: true,
+    }).start(() => {
+      setTimeout(() => {
+        Animated.timing(this.fadeAnimation, {
+          toValue: 0,
+          duration: 500,
+          useNativeDriver: true,
+        }).start()
+      }, 1500)
+
+    });
+
+  }
 
   render() {
     const { params } = this.props.navigation.state;
@@ -113,39 +101,41 @@ class SpellCheckScreen extends React.Component {
 
 
     return (
-      <View style={{flex: 1}}>
+      <View style={{ flex: 1 }}>
         <ScrollView>
-<View style={{padding: 15}}>
-  {this.state.resultText == "" ? (
-    <ActivityIndicator size="large" color="#1479d9" />
-  ) : (
-    <Text style={{fontSize: 18}}>{this.state.resultText}</Text>
-  )}
-</View>
+          <View style={{ padding: 15 }}>
+            {this.state.resultText == "" ? (
+              <ActivityIndicator size="large" color="#1479d9" />
+            ) : (
+                <Text style={{ fontSize: 18 }}>{this.state.resultText}</Text>
+              )}
+          </View>
 
-</ScrollView>
+        </ScrollView>
 
-<View style={styles.animatedContainer}>
+        <View style={styles.animatedContainer}>
 
-        <Animated.View style={[styles.container, { opacity: this.fadeAnimation }]}>
-          <Text style={styles.text}>Text was copied</Text>
-        </Animated.View>
+          <Animated.View style={[styles.container, { opacity: this.fadeAnimation }]}>
+            <Text style={styles.text}>Text was copied</Text>
+          </Animated.View>
 
         </View>
 
-{this.state.resultText !== "" ? (
-  <View style={{ margin: 20, position: 'absolute', bottom: 0, right: 0, flexDirection: 'row'}}>
-<AntIcon name='copy1' style={{backgroundColor:'#1479d9', fontSize: 32, color: 'white',
-        padding: 10, borderRadius: 50, marginRight: 8}} onPress={() => {
-          Clipboard.setString(this.state.resultText),
-          this.animationStart()
-         }}/>
-         </View>
-) : (
-  <View></View>
-)}
+        {this.state.resultText !== "" ? (
+          <View style={{ margin: 20, position: 'absolute', bottom: 0, right: 0, flexDirection: 'row' }}>
+            <AntIcon name='copy1' style={{
+              backgroundColor: '#1479d9', fontSize: 32, color: 'white',
+              padding: 10, borderRadius: 50, marginRight: 8
+            }} onPress={() => {
+              Clipboard.setString(this.state.resultText),
+                this.animationStart()
+            }} />
+          </View>
+        ) : (
+            <View></View>
+          )}
 
-</View>
+      </View>
     );
   }
 }
@@ -172,4 +162,4 @@ const styles = StyleSheet.create({
 });
 
 
-  export default SpellCheckScreen;
+export default SpellCheckScreen;
